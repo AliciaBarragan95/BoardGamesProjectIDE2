c87a6aec7cf38e332137c4fad319a0524e0ab3a9
AliciaBarragan95 <aliciabarragan95@gmail.com>
2022-08-17 11:52:52 +0200

    initial commit

---
+++ src/main/java/com/boardGamesProjectDemo/BoardGamesProject/BoardGamesProjectApplication.java
@@ -0,0 +1,13 @@
+package com.boardGamesProjectDemo.BoardGamesProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardGamesProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardGamesProjectApplication.class, args);
+	}
+
+}
